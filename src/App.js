57c50e6652ed415f92fc944aebdccfaaff9d0060
aiyus <aiyusboss@gmail.com>
2023-04-26 14:03:20 +0100

    financial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/header/header";
 import Hero from "./components/hero/hero";
+import Financial from "./components/finanacial_stock/financial_stock";
 function App() {
   return (
-    <div><Header/>
-   <Hero/></div>
+    <div>
+      <Header/>
+      <Hero/>
+      <Financial/>
+   </div>
    
   );
 }
---
+++ src/components/finanacial_stock/financial_stock.js
@@ -0,0 +1,60 @@
+import React from "react";
+import arroww from '../brassImg/Rectangle 6.svg'
+import lock from '../brassImg/Rectangle 7.svg'
+import hand from '../brassImg/Rectangle 8.svg'
+
+const Financial = () => {
+  return (
+    <div className="financial_container">
+        <div className="financial_heading" style={{textAlign:"center"}}>
+        <h1 style={{paddingBottom:'15px'}}> The complete financial stack for your business</h1 >
+      <p>Top-end financial tools to help your business with its financial operations and cash flow.</p>
+      <p>We help companies of all types work better, save time and money with a simply better banking service that works.</p>
+        </div>
+     
+      <div className="financial_flex">
+        <div className="speed">
+          <div className="speed_img">
+            <img src={arroww} alt="speed"/>
+          </div>
+          <div className="speed_text">
+            <h3>
+            Speed
+            </h3>
+            <p>
+            Open a commercial-grade current account from anywhere in 10 minutes
+            </p>
+          </div>
+        </div>
+        <div className="speed">
+        <div className="speed_img">
+            <img src={lock} alt="security"/>
+          </div>
+          <div className="speed_text">
+            <h3>
+            Security
+            </h3>
+            <p>
+            Rely on bank-grade security with all your monies fully insured by NDIC
+            </p>
+          </div>
+        </div>
+        <div className="speed">
+        <div className="speed_img">
+            <img src={hand} alt="support"/>
+          </div>
+          <div className="speed_text">
+            <h3>
+            Support
+            </h3>
+            <p>
+            Get access to a trusted support team and resources whenever you want
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Financial;
