1ff6939828f3223911199d3bf6ef1325287b1906
aiyus <aiyusboss@gmail.com>
2023-04-28 09:31:12 +0100

    toogle active in need

--- src/components/needs/needs.js
+++ src/components/needs/needs.js
@@ -1,7 +1,13 @@
 import React from "react";
 import file from '../brassImg/Rectangle 10.png'
+import { useState } from "react";
 
 const Needs = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const handleClick = (index) => {
+      setActiveIndex(index);}
+    
   return (
     <div className="needs_container">
       <div className="needs_heading">
@@ -9,28 +15,28 @@ const Needs = () => {
       </div>
       <div className="needs_flex">
         <div className="needs_left">
-          <div className="Payments">
+          <div className={activeIndex === 0 ? "active" : "myneeds"} onClick={() => handleClick(0)}>
             <h4>Payments</h4>
             <p>Create instant, scheduled and recurring payments easily</p>
           </div>
 
-          <div className="Invoices">
+          <div className={activeIndex === 1 ? "active" : "myneeds"} onClick={() => handleClick(1)}>
             <h4>Invoices</h4>
             <p>Send, receive and reconcile invoices all on one platform</p>
           </div>
 
-          <div className="Cashflow ">
+          <div className={activeIndex === 2 ? "active" : "myneeds"} onClick={() => handleClick(2)}>
             <h4>Cashflow Support</h4>
             <p>
               Move your business forward with insight and financila resources
             </p>
           </div>
-          <div className="cross">
+          <div className= {activeIndex === 3 ? "active" : "myneeds"} onClick={() => handleClick(3)}>
             <h4>Cross-platform availability</h4>
             <p>Bank everywhere. Available on iOS, Android and Slack</p>
           </div>
 
-          <div className="team">
+          <div className={activeIndex === 4 ? "active" : ""} onClick={() => handleClick(4)}>
             <h4>Team banking</h4>
             <p>Work with your team to run your entire financial opertations</p>
           </div>
